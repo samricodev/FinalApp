67bf96a2f8c9e160121221b8527c5b322eccc574
samricodev <samuel.isaac.rico.estrada@gmail.com>
2023-06-16 13:37:44 -0600

    home menu added

--- app/src/main/java/com/example/finalapp/MainActivity.java
+++ app/src/main/java/com/example/finalapp/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent intent = new Intent(this, ActivityLogin.class);
+        Intent intent = new Intent(this, ActivityMenu.class);
         startActivity(intent);
     }
 }

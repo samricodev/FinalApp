8e88cb81e6465460da0013afe15ca4942208d47f
samricodev <samuel.isaac.rico.estrada@gmail.com>
2023-06-16 22:23:41 -0600

    Reseña java done

--- app/src/main/java/com/example/finalapp/fragment_resena.java
+++ app/src/main/java/com/example/finalapp/fragment_resena.java
@@ -7,6 +7,10 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -24,6 +28,9 @@ public class fragment_resena extends Fragment {
     private String mParam1;
     private String mParam2;
 
+    private EditText nombre, titulo, fecha, descrip;
+    private CheckBox c1, c2, c3, c4, c5;
+    private Button btnEnviar;
     public fragment_resena() {
         // Required empty public constructor
     }
@@ -59,6 +66,65 @@ public class fragment_resena extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_resena, container, false);
+        View view = inflater.inflate(R.layout.fragment_resena, container, false);
+
+        nombre = view.findViewById(R.id.txtNombre_resena);
+        titulo = view.findViewById(R.id.txtTitulo_resena);
+        fecha = view.findViewById(R.id.txtFecha_resena);
+        descrip = view.findViewById(R.id.txtDescripcion_resena);
+        c1 = view.findViewById(R.id.cb1_resena);
+        c2 = view.findViewById(R.id.cb2_resena);
+        c3 = view.findViewById(R.id.cb3_resena);
+        c4 = view.findViewById(R.id.cb4_resena);
+        c5 = view.findViewById(R.id.cb5_resena);
+
+        btnEnviar = view.findViewById(R.id.btnEnviar);
+
+        btnEnviar.setOnClickListener(v -> enviarRes(view));
+
+        return view;
+    }
+
+    public void enviarRes(View view){
+        int starts = 0;
+
+        if(c1.isChecked()){
+            starts++;
+        }
+        if(c2.isChecked()){
+            starts++;
+        }
+        if(c3.isChecked()){
+            starts++;
+        }
+        if(c4.isChecked()){
+            starts++;
+        }
+        if(c5.isChecked()){
+            starts++;
+        }
+
+        Reseña res = new Reseña(nombre.getText().toString(), titulo.getText().toString(), fecha.getText().toString()
+                    , descrip.getText().toString(), starts);
+
+        String response = "Reseña\n" + "Nombre: " + res.getNombre() + " Titulo: " + res.getTitulo()
+                + " Fecha: " + res.getFecha() + " Descripcion: " + res.getDescripcion()
+                + " Calificacion: " + res.getCalificacion() + " estrellas";
+
+        limpiarResena();
+
+        Toast.makeText(this.getContext(), response, Toast.LENGTH_LONG).show();
+    }
+
+    public void limpiarResena(){
+        nombre.setText(null);
+        titulo.setText(null);
+        fecha.setText(null);
+        descrip.setText(null);
+        c1.setChecked(false);
+        c2.setChecked(false);
+        c3.setChecked(false);
+        c4.setChecked(false);
+        c5.setChecked(false);
     }
 }
